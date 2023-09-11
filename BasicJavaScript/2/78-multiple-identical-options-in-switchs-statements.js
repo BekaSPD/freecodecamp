cc5a49be3e93d948cbf7e9c59ab7143bad03dcd7
BekaSPD <bekm88@gmail.com>
2023-09-11 09:17:20 +0600

    78 Multiple Identical Options in Switch Statements

---
+++ BasicJavaScript/2/78-multiple-identical-options-in-switchs-statements.js
@@ -0,0 +1,60 @@
+// Multiple Identical Options in Switch Statements
+// If the break statement is omitted from a switch statement's case, the following case statement(s) are executed until a break is encountered. If you have multiple inputs with the same output, you can represent them in a switch statement like this:
+//
+//     let result = "";
+//     switch (val) {
+//         case 1:
+//         case 2:
+//         case 3:
+//             result = "1, 2, or 3";
+//             break;
+//         case 4:
+//             result = "4 alone";
+//     }
+//     Cases for 1, 2, and 3 will all produce the same result.
+//
+//         Write a switch statement to set answer for the following ranges:
+//         1-3 - Low
+//         4-6 - Mid
+//         7-9 - High
+//
+//         Note: You will need to have a case statement for each number in the range.
+//
+//         Tests
+//         Waiting:sequentialSizes(1) should return the string Low
+//         Waiting:sequentialSizes(2) should return the string Low
+//         Waiting:sequentialSizes(3) should return the string Low
+//         Waiting:sequentialSizes(4) should return the string Mid
+//         Waiting:sequentialSizes(5) should return the string Mid
+//         Waiting:sequentialSizes(6) should return the string Mid
+//         Waiting:sequentialSizes(7) should return the string High
+//         Waiting:sequentialSizes(8) should return the string High
+//         Waiting:sequentialSizes(9) should return the string High
+//         Waiting:You should not use any if or else statements
+//         Waiting:You should have nine case statements
+
+//Solution
+
+function sequentialSizes(val) {
+    let result = "";
+
+    switch (val) {
+        case 1:
+        case 2:
+        case 3:
+            result = "Low";
+            break;
+        case 4:
+        case 5:
+        case 6:
+            result = "Mid";
+            break;
+        case 7:
+        case 8:
+        case 9:
+            result = "High";
+            break;
+    }
+
+    return result;
+}
