ffeda23e421408b77b0b148222fdb268707f8f9a
BekaSPD <bekm88@gmail.com>
2023-09-11 09:12:54 +0600

    77 Adding a Default Option in Switch Statements

---
+++ BasicJavaScript/77-adding-a-default-option-in-switch-statements.js
@@ -0,0 +1,58 @@
+// Adding a Default Option in Switch Statements
+// In a switch statement you may not be able to specify all possible values as case statements. Instead, you can add the default statement which will be executed if no matching case statements are found. Think of it like the final else statement in an if/else chain.
+//
+//     A default statement should be the last case.
+//
+//     switch (num) {
+// case value1:
+//         statement1;
+//     break;
+// case value2:
+//         statement2;
+//     break;
+// ...
+// default:
+//     defaultStatement;
+//     break;
+// }
+//     Write a switch statement to set answer for the following conditions:
+//         a - apple
+//         b - bird
+//         c - cat
+//         default - stuff
+//
+//             Tests
+//             Waiting:switchOfStuff("a") should return the string apple
+//             Waiting:switchOfStuff("b") should return the string bird
+//             Waiting:switchOfStuff("c") should return the string cat
+//             Waiting:switchOfStuff("d") should return the string stuff
+//             Waiting:switchOfStuff(4) should return the string stuff
+//             Waiting:You should not use any if or else statements
+//             Waiting:You should use a default statement
+//             Waiting:You should have at least 3 break statements
+
+//Solution
+
+function switchOfStuff(val) {
+    var result = "";
+
+    switch (val) {
+        case "a":
+            result = "apple";
+            break;
+        case "b":
+            result = "bird";
+            break;
+        case "c":
+            result = "cat";
+            break;
+        default:
+            result = "stuff";
+            break;
+    }
+
+    return result;
+}
+
+
+switchOfStuff(1);
